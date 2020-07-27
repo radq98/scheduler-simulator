b0271e059d3834537efac597730dd3573c850e59
radq98 <radekzegadlo@gmail.com>
2020-07-27 23:19:42 +0200

    Generator added

--- app.py
+++ app.py
@@ -9,14 +9,19 @@ from flask import (
 )
 from datetime import datetime, timedelta
 from random import randint
+import json
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "5Q8wfQqmx67Ez0wondbxSeYXS8mE2gob"
 app.config["DEBUG"] = True
 
-#metadata
-appversion = "0.1"
-footertext = "Radosław Zegadło - Wroclaw University of Science 2020"
+# metadata
+metadata = None
+with open("meta.json", "r") as meta_file:
+    metadata = json.load(meta_file)
+appversion = metadata['appversion']
+footertext = metadata['footertext']
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -27,6 +32,7 @@ def index():
     }
     return render_template("index.html", context=context)
 
+
 @app.route("/results")
 def results():
     context = {
@@ -34,6 +40,34 @@ def results():
         "footertext": footertext,
     }
     return render_template("results.html", context=context)
+
+
+@app.route("/generator", methods=["POST"])
+def generator():
+    form = request.form
+    newDataset = []
+    if form.get("number-of-processes") != None:
+        numberOfProcesses = int(form.get("number-of-processes"))
+        #randomSeed = int(form.get("random-seed"))
+        #randomizePriority = form.get("randomize-priority")
+        for x in range(numberOfProcesses):
+            newDataset.append({
+                "id": x, 
+                "period-time": randint(1,100), 
+                "priority": randint(1,100)
+                })
+    print(newDataset)
+    if newDataset != []:
+        with open("./datasets/tmp.json", "w",) as f:
+            json.dump(newDataset, f, indent=4)
+
+    form = None
+    context = {
+        "appversion": appversion,
+        "footertext": footertext,
+    }
+    return render_template("generator.html", context=context)
+
 
 if __name__ == "__main__":
     app.run()
