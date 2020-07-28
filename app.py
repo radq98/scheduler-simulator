efd9ddd1657ee1f8079e9eea6281d714eae613ea
radq98 <radekzegadlo@gmail.com>
2020-07-28 23:01:29 +0200

    Generator update

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from flask import (
 from datetime import datetime, timedelta
 from random import randint
 import json
+import os
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "5Q8wfQqmx67Ez0wondbxSeYXS8mE2gob"
@@ -44,30 +45,45 @@ def results():
 
 @app.route("/generator", methods=["POST", "GET"])
 def generator():
+    #datasetsBase = [
+    #    name
+    #    for name in os.listdir("./datasets")
+    #    if os.path.isdir(os.path.join("./datasets", name))
+    #]
+    datasetsBase = os.listdir("./datasets/")
+    print(datasetsBase)
     if request.method == 'POST':
-        newDataset = []
+        newDataset = {"settings": None, "data": []}
         form = request.form
         datasetSettings = {
             "numberOfProcesses": int(form.get("number-of-processes")),
             "minimumProcessDuration": int(form.get("minimum-process-duration")),
             "maximumProcessDuration": int(form.get("maximum-process-duration")),
+            "processDispersionFactor": int(form.get("process-dispersion-factor")),
             "randomizePriority": bool(form.get("randomize-priority")),
             "randomSeed": int(form.get("random-seed")),
             "numberOfPriorityLevels": int(form.get("number-of-priority-levels"))
         }
-        newDataset.append(datasetSettings)
+        newDataset['settings'] = datasetSettings
         if form.get("number-of-processes") != None:
+            lastArrive = 0
+            arriveTime = 0
             for x in range(datasetSettings.get('numberOfProcesses')):
                 if datasetSettings['randomizePriority']:
                     priority = randint(1,datasetSettings['numberOfPriorityLevels'])
                 else:
                     priority = 1
-                newDataset.append({
+                if lastArrive == 0:
+                    lastArrive += 1
+                else:
+                    arriveTime = randint(lastArrive, lastArrive + int((datasetSettings['processDispersionFactor']/100) * datasetSettings['maximumProcessDuration']))
+                    lastArrive = arriveTime
+                newDataset['data'].append({
                     "PID": x, 
+                    "arrive-time": arriveTime,
                     "period-time": randint(datasetSettings['minimumProcessDuration'],datasetSettings['maximumProcessDuration']), 
                     "priority": priority
                     })
-        print(newDataset)
         if newDataset != []:
             with open("./datasets/tmp.json", "w",) as f:
                 json.dump(newDataset, f, indent=4)
@@ -76,6 +92,7 @@ def generator():
     context = {
         "appversion": appversion,
         "footertext": footertext,
+        "datasetsBase": datasetsBase
     }
     return render_template("generator.html", context=context)
 
